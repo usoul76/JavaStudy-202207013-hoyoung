3fbb24700a72eb6241f8e840b0c0e8fab5554d92
usoul76 <jangna1336@gmail.com>
2022-07-17 17:07:50 +0900

    챕터 1 출력

---
+++ src/j01_출력/MyInfo.java
@@ -0,0 +1,25 @@
+package j01_출력;
+
+public class MyInfo {
+
+	public static void main(String[] args) {
+		char name1 = '김';
+		char name2 = '호';
+		char name3 = '영';
+		String name = "김호영";
+		
+		int age = 16;
+		
+		String address = "부산 수영구 망미동";
+		
+		System.out.print("이름: ");
+		System.out.println(name);
+		System.out.print("나이: ");
+		System.out.println(age);
+		System.out.print("주소: ");
+		System.out.println(address);
+		System.out.println();
+
+	}
+
+}
---
+++ src/j01_출력/printTest.java
@@ -0,0 +1,12 @@
+package j01_출력;
+
+public class printTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, Java!\n");
+		System.out.println("김호영");
+		
+	}
+
+}
