e7e810de3758a48737db2284373d57aba33d8e79
usoul76 <jangna1336@gmail.com>
2022-07-21 10:05:46 +0900

    챕터 7 반복

--- src/j07_반복/For2.java
+++ src/j07_반복/For2.java
@@ -5,16 +5,16 @@ public class For2 {
 	public static void main(String[] args) {
 		
 		for(int i = 0; i < 8; i++) {
-			int dan =  i + 2;
+			int dan = i + 2;
 			System.out.println(dan + "단");
-		}
-		
-		for(int j = 0; j < 9; j++) {
-			int num = j + 1;
+			
+			for(int j = 0; j < 9; j++) {
+				int num = j + 1;
 				System.out.println(dan + " x " + num + " = " + (dan * num));
+			}
+			
+			System.out.println();
 		}
-
-		System.out.println();
 	}
 
 }
