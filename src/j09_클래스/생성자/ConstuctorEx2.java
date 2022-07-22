80850bae9a10a4d8ddcd72edf54b5a5de7009391
usoul76 <jangna1336@gmail.com>
2022-07-22 12:15:32 +0900

    챕터 9 클래스.생성자

---
+++ src/j09_클래스/생성자/ConstuctorEx2.java
@@ -0,0 +1,23 @@
+package j09_클래스.생성자;
+
+public class ConstuctorEx2 {
+	
+	String name;
+	int age;
+	
+	public ConstuctorEx2() {}
+	
+	public ConstuctorEx2(String name) {
+		this.name = name;
+	}
+	
+	public ConstuctorEx2(int age) {
+		this.age = age;
+	}
+	
+	public ConstuctorEx2(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+}
---
+++ src/j09_클래스/생성자/ConstuctorEx2Main2.java
@@ -0,0 +1,8 @@
+package j09_클래스.생성자;
+
+public class ConstuctorEx2Main2 {
+	
+	public static void main(String[] args) {
+		ConstuctorEx2 constuctorEx2 = new ConstuctorEx2();
+	}
+}
