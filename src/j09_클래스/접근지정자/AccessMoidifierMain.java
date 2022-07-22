040380e02989e0b29d4e5d34911be60c215c9e84
usoul76 <jangna1336@gmail.com>
2022-07-22 12:53:17 +0900

    챕터 9 접근 클래스.접근지정자

---
+++ src/j09_클래스/접근지정자/AccessMoidifierMain.java
@@ -0,0 +1,15 @@
+package j09_클래스.접근지정자;
+
+import j09_클래스.접근지정자.a.TestA_2;
+import j09_클래스.접근지정자.b.TestB;
+
+public class AccessMoidifierMain {
+
+	public static void main(String[] args) {
+		TestA_2 testA_2 = new TestA_2();
+		testA_2.testA2();
+		
+		TestB testB = new TestB();
+	}
+
+}
