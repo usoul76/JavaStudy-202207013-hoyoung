222353e295ab8a92e60b0fbe4f97eb6b5b7355fc
usoul76 <jangna1336@gmail.com>
2022-07-27 09:04:45 +0900

    챕터 9 클래스2

--- src/j09_클래스2/StudentService.java
+++ src/j09_클래스2/StudentService.java
@@ -13,13 +13,13 @@ public class StudentService {
 	}
 	
 	private String inputSelect() {
-		System.out.println("매뉴 선택 >> ");
+		System.out.print("메뉴선택 >> ");
 		return scanner.nextLine();
 	}
 	
-	private void printSelectErrirMessage() {
-		System.out.println("해당 메뉴는 선택 할 수 없습니다");
-		System.out.println("다시 입력하세요");
+	private void printSelectErrorMessage() {
+		System.out.println("해당 메뉴는 선택 할 수 없습니다.");
+		System.out.println("다시 입력하세요.");
 	}
 	
 	public boolean showMainMenu() {
@@ -28,11 +28,11 @@ public class StudentService {
 		System.out.println("[학생 등록 시스템]");
 		System.out.println("1. 학생 등록");
 		System.out.println("2. 학생 조회");
-		System.out.println("q. 프로그램 종료");
-		
+		System.out.println("e. 프로그램 종료");
+	
 		select = inputSelect();
 		
-		if(select.equals("1")){
+		if(select.equals("1")) {
 			Student student = addStudent();
 			if(student != null) {
 				System.out.println(student.toString());
@@ -40,59 +40,58 @@ public class StudentService {
 		}else if(select.equals("2")) {
 			while(showStudentSearch()) {}
 			
-		}else if(select.equals("q")) {
+		}else if(select.equals("e")) {
 			System.out.println("프로그램 종료중...");
 			return false;
 		}else {
-			printSelectErrirMessage();
+			printSelectErrorMessage();
 		}
 		
 		System.out.println();
 		
 		return true;
-		
 	}
 	
 	private Student addStudent() {
-		for(int i = 0; i <studentArray.length; i++) {
+		for(int i = 0; i < studentArray.length; i++) {
 			if(studentArray[i] != null) {
 				continue;
 			}
-			studentArray[i] = inputStudnet();
-			System.out.println("[" +studentArray[i].getStudentName() + "] 학생 등록 완료");
+			studentArray[i] = inputStudent();
+			System.out.println("[ " + studentArray[i].getStudentName() + " ] 학생 등록 완료");
 			return studentArray[i];
-			
 		}
-		System.out.println("더 이상 학생을 등록 할 수 없습니다");
+		
+		System.out.println("더 이상 학생을 등록 할 수 없습니다.");
 		
 		return null;
 	}
 	
-	private Student inputStudnet() {
+	private Student inputStudent() {
 		String studentName = null;
 		int studentYear = 0;
-		String studentAddress = null;;
+		String studentAddress = null;
 		String studentPhone = null;
 		
 		System.out.println("[학생 등록]");
-		System.out.println("이름 >> ");
+		System.out.print("이름 >> ");
 		studentName = scanner.nextLine();
 		
-		System.out.println("학년 >> ");
+		System.out.print("학년 >> ");
 		studentYear = scanner.nextInt();
 		scanner.nextLine();
 		
-		System.out.println("주소 >> ");
+		System.out.print("주소 >> ");
 		studentAddress = scanner.nextLine();
 		
-		System.out.println("연락처 >> ");
+		System.out.print("연락처 >> ");
 		studentPhone = scanner.nextLine();
 		
 		return new Student(studentName, studentYear, studentAddress, studentPhone);
 	}
 	
 	private boolean showStudentSearch() {
-		String select  = null;
+		String select = null;
 		
 		System.out.println("[학생 조회]");
 		System.out.println("1. 학생 전체 조회");
@@ -102,29 +101,31 @@ public class StudentService {
 		select = inputSelect();
 		
 		if(select.equals("1")) {
+			showStudentList();
 			
 		}else if(select.equals("2")) {
 			showSearchStudent();
 			
 		}else if(select.equals("b")) {
-			System.out.println("이전 매뉴로 돌아갑니다");
+			System.out.println("이전 메뉴로 돌아갑니다.");
 			return false;
 		}else {
-			printSelectErrirMessage();
+			printSelectErrorMessage();
 		}
 		System.out.println();
 		return true;
+		
 	}
 	
 	private void showStudentList() {
 		System.out.println("[학생 전체 조회]");
 		System.out.println("이름\t\t학년\t\t주소\t\t연락처");
-		for(int i = 0; i <studentArray.length; i++) {
+		for(int i = 0; i < studentArray.length; i++) {
 			if(studentArray[i] != null) {
 				System.out.println(studentArray[i].getStudentName() + "\t\t" 
 						+ studentArray[i].getStudentYear() + "\t\t" 
 						+ studentArray[i].getStudentAddress() + "\t\t"
-						+ studentArray[i].getStudentPhone());
+						+ studentArray[i].getStudentPhone());				
 			}
 		}
 		System.out.println();
@@ -134,12 +135,12 @@ public class StudentService {
 		String searchName = null;
 		
 		System.out.println("[학생 이름으로 검색]");
-		System.out.println("이름 >>");
+		System.out.print("이름 >> ");
 		searchName = scanner.nextLine();
 		
-		Student serStudent = searchStudentByStudentName(searchName);
-		if(serStudent != null) {
-			System.out.println(serStudent.toString());
+		Student searchStudent = searchStudentByStudentName(searchName);
+		if(searchStudent != null) {
+			System.out.println(searchStudent.toString());
 		}
 		
 	}
@@ -148,12 +149,13 @@ public class StudentService {
 		for(int i = 0; i < studentArray.length; i++) {
 			if(studentArray[i] != null) {
 				String studentName = studentArray[i].getStudentName();
-				if(searchName.equals(searchName)) {
+				if(searchName.equals(studentName)) {
 					return studentArray[i];
 				}
 			}
 		}
-		System.out.println("해당 이름의 학생은 존재하지 않습니다");
+		System.out.println("해당 이름의 학생은 존재하지 않습니다.");
 		return null;
 	}
+
 }
