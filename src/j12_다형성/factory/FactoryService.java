e09d0732acd13f8152e69796c8f13f7e18b5c113
usoul76 <jangna1336@gmail.com>
2022-07-27 12:49:10 +0900

    챕터 12  다형성

---
+++ src/j12_다형성/factory/FactoryService.java
@@ -0,0 +1,19 @@
+package j12_다형성.factory;
+
+import j11_상속.Factory.Factory;
+
+public class FactoryService {
+	private Factory factory;
+	
+	public FactoryService(Factory factory) {
+		this.factory = factory;
+	}
+	
+	public void factoryStart() {
+		factory.start();
+	}
+	
+	public void factoryStop() {
+		factory.stop();
+	}
+}
---
+++ src/j12_다형성/추상/Animal.java
@@ -0,0 +1,14 @@
+package j12_다형성.추상;
+
+/*
+ * 추상
+ * 추상 메소드가 하나라도 포함되어 있는 클래스는 
+ * 무조건 추상 클래스로 정의해야한다
+ * 
+ */
+
+public abstract class Animal {
+
+	//abstract(추상 메소드)
+	public abstract void move();
+}
