abe43fb90bcb368868ca17cb5e76640ef4e59dc3
usoul76 <jangna1336@gmail.com>
2022-07-29 12:47:20 +0900

    챕터 6 조건

--- src/j06_조건/SwitchCase2.java
+++ src/j06_조건/SwitchCase2.java
@@ -3,34 +3,36 @@ package j06_조건;
 public class SwitchCase2 {
 
 	public static void main(String[] args) {
-		int score = 85;
-		boolean breakFlag = false;
-		
+		int score = 100;
+		boolean breakFlag = true;
 		
 		switch(score / 10) {
 			case 10:
 			case 9:
 				System.out.println("A");
 				if(breakFlag) {
-					break;
+					break;					
 				}
 			case 8:
 				System.out.println("B");
 				if(breakFlag) {
-					break;
+					break;					
 				}
 			case 7:
 				System.out.println("C");
 				if(breakFlag) {
-					break;
+					break;					
 				}
 			case 6:
 				System.out.println("D");
+				if(breakFlag) {
+					break;					
+				}
 			default:
 				System.out.println("F");
 		}
-		
 
 	}
 
 }
+
